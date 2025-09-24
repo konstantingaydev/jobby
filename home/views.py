1ab2fd166176f69d7bcf6bbdaf06eb1871cc6256
konstantingaydev <konstantingaydev16@gmail.com>
2025-09-23 20:30:08 -0400

    created home page base template

--- home/views.py
+++ home/views.py
@@ -1,5 +1,6 @@
 from django.shortcuts import render
-
-# Create your views here.
 def index(request):
-    return render(request, 'home/index.html')
+    template_data = {}
+    template_data['title'] = 'Jobby'
+    return render(request, 'home/index.html', {
+        'template_data': template_data})
